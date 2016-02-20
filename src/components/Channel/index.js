58383bf571b30ca5104a9eeb592100e656150514
enricostano <ocirne.onats@gmail.com>
2016-02-20 18:34:21 +0100

    Add react-router

---
+++ src/components/Channel/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Channel extends Component {
+  render() {
+    return <p>{this.props}</p>;
+  }
+};
+
+export default Channel;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import ChannelList from 'components/ChannelList';
+import { Router, Route, hashHistory } from 'react-router';
+import ChannelsContainer from 'containers/ChannelsContainer';
+import Channel from 'components/Channel';
 
 var div = document.createElement('div');
 document.body.appendChild(div);
 
-var request = new XMLHttpRequest();
-request.open('GET', 'http://somafm.com/channels.json');
-request.addEventListener('load', function() {
-  var response = JSON.parse(this.responseText);
-  render(
-    <ChannelList channels={response.channels}/>,
-    div
-  );
-});
-request.send();
+render((
+  <Router history={hashHistory}>
+    <Route path='/' component={ChannelsContainer}>
+      <Route path='channel' component={Channel}>
+      </Route>
+    </Route>
+  </Router>
+), div);
